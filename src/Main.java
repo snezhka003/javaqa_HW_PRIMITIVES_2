909eddf8ded563d69cd91ca99bb2e36445e4dc9a
snezhka003 <you@example.com>
2025-09-18 17:24:08 +0300

    Add info to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int startAmount = 429;
+        int sum = 581;
 
+//        int bonus;
+//
+//        if (sum > 1000) {
+//            bonus = sum / 100;
+//        } else {
+//            bonus = 0;
+//        }
+
+        int bonus = sum > 1000 ? sum / 100 : 0;
+
+        int finishAmount = startAmount + sum + bonus;
+
+        System.out.println("Бонус: " + bonus + " руб." + " Итоговая сумма на счету: " + finishAmount + " руб.");
     }
 }
